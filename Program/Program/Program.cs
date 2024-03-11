c6d54ccfa2443770878709726fa5a34027663467
s29309 <s29309@pjwstk.edu.pl>
2024-03-11 13:26:02 +0100

    nowy brancz

---
+++ Program/Program/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
+    }
+
+    public static double Avg(int[] arr)
+    {
+        double sum = 0;
+        foreach (int k in arr)
+        {
+            sum += k;
+        }
+
+        return sum / arr.Length;
+    }
+}
