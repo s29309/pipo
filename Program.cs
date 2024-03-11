8b3849fb735f736dbbf9a8a996f3d44eaa8428bb
Eryk Zieliński <s29309@pjwstk.edu.pl>
2024-03-11 13:04:49 +0100

    Modyfikacja 3

--- Program.cs
+++ Program.cs
@@ -3,6 +3,6 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("dziendoberek");
+        Console.WriteLine("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
     }
 }
